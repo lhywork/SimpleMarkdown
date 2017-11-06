9994492d420e480ed9906d71e9cfe5901c838b78
zhuangZhou <1851055435@163.com>
2017-11-07 07:46:27 +0800

    优化css

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 hljs.initHighlightingOnLoad();
 
-$(function () {
+$(function() {
     $('.mdList li')
-        .on('click', function () {
+        .on('click', function() {
             $(this)
                 .addClass('active')
                 .siblings()
@@ -14,78 +14,79 @@ $(function () {
                 .removeClass('none');
         });
 
-    $('.mdList .deleteMd').on('mouseenter', function () {
+    $('.mdList .deleteMd').on('mouseenter', function() {
         $(this)
             .parents('li')
-            .css({opacity: 0.8});
+            .css({ opacity: 0.8 });
     });
-    $('.mdList .deleteMd').on('mouseleave', function () {
+    $('.mdList .deleteMd').on('mouseleave', function() {
         $(this)
             .parents('li')
-            .css({opacity: 1});
+            .css({ opacity: 1 });
     });
 
     //功能按键 粗体设置
-    $('abbr').on('click',function(){
-        try {
-            clearTimeout(timer);
-        } catch (e) {};
-    });
-    
-    $('#boldSet').on('click', function () {
+
+    $('#boldSet').on('click', function() {
         var str = $('#editArea').val();
         $('#editArea').val(str + '## text ## \n');
+        saveMd();
     });
 
     //斜体设置
-    $('#xieSet').on('click', function () {
+    $('#xieSet').on('click', function() {
 
         var str = $('#editArea').val();
         $('#editArea').val(str + '*text* \n');
+        saveMd();
     });
 
     //超链接设置
-    $('#httpSet').on('click', function () {
+    $('#httpSet').on('click', function() {
         var str = $('#editArea').val();
         $('#editArea').val(str + '[](http://) \n');
+        saveMd();
     });
 
     //图片设置
-    $('#imgSet').on('click', function () {
+    $('#imgSet').on('click', function() {
         var str = $('#editArea').val();
         $('#editArea').val(str + '![Alt text]() \n');
+        saveMd();
     });
 
     //表格设置
-    $('#tableSet').on('click', function () {
+    $('#tableSet').on('click', function() {
         var str = $('#editArea').val();
         $('#editArea').val(str + '| a | b | c | \n | --- | --- | --- | \n | 1 | 2 | 3 | \n');
+        saveMd();
     });
 
     //预览设置
-    $('#previewSet').on('click', function () {
+    $('#previewSet').on('click', function() {
         var show = $(this).attr('data-show');
         if (show == 'false') {
             $('.editMark .right').removeClass('none');
             $(this)
                 .parents('.left')
-                .css({width: '50%'});
+                .css({ width: '50%' });
             $(this).attr('data-show', true);
         } else {
             $('.editMark .right').addClass('none');
             $(this)
                 .parents('.left')
-                .css({width: '100%'});
+                .css({ width: '100%' });
             $(this).attr('data-show', false);
         }
     });
 
-    var timer = null;
-    $('#editArea').on("keyup", function () {
-        try {
-            clearTimeout(timer);
-        } catch (e) {};
-        timer = setTimeout(saveMd, 3000);
+
+    $('#editArea').on("keyup", function() {
+        saveMd();
+    });
+
+    $('#markTitle').on("keyup", function() {
+        saveMd();
     });
 
 });
@@ -96,9 +97,9 @@ function saveMd() {
     $.post('/save', {
         markdown: markdown,
         title: title
-    }, function (res) {
+    }, function(res) {
         $('.right').html(res.html);
-        $('pre code').each(function (i, block) {
+        $('pre code').each(function(i, block) {
             hljs.highlightBlock(block);
         });
     }, 'json');
--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,30 @@ const router = express.Router();
 const marked = require('marked');
 
 marked.setOptions({
-  renderer: new marked.Renderer(),
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pedantic: false,
-  sanitize: true,
-  smartLists: true,
-  smartypants: false
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
 });
 
 router.get('/', (req, res) => {
-  let data = {
-    ab: 'a'
-  }
-  res.render('index', data);
+    let data = {
+        ab: 'a'
+    }
+    res.render('index', data);
 });
 
 router.post('/save', (req, res) => {
-  let title = req.body.title;
-  let markdown = req.body.markdown;
-  let html = `# ${title} # \n`  + markdown;
-  let data ={};
-  data.html = marked(html);
-  res.json(data);
+    let title = req.body.title;
+    let markdown = req.body.markdown;
+    let html = `# ${title} # \n` + markdown;
+    let data = {};
+    data.html = marked(html);
+    res.json(data);
 });
 
 module.exports = router;
